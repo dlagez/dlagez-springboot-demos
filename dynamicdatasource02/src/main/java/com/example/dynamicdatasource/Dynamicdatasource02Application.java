e9ca0ca61602f1d2af65abbc41baf935976c8065
roczhang <1587839905@qq.com>
2021-08-23 17:17:47 +0800

    多数据源测试完，读写分离测试开始

---
+++ dynamicdatasource02/src/main/java/com/example/dynamicdatasource/Dynamicdatasource02Application.java
@@ -0,0 +1,13 @@
+package com.example.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dynamicdatasource02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dynamicdatasource02Application.class, args);
+    }
+
+}
---
+++ dynamicdatasource02/src/test/java/com/example/dynamicdatasource/Dynamicdatasource02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dynamicdatasource02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

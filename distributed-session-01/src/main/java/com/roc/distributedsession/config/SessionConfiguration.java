ffeecadb2fe289efe1f24708c18e546f93032de5
roczhang <1587839905@qq.com>
2021-11-16 17:00:33 +0800

    session jpa

--- distributed-session-01/src/main/java/com/roc/distributedsession/config/SessionConfiguration.java
+++ distributed-session-01/src/main/java/com/roc/distributedsession/config/SessionConfiguration.java
@@ -1,2 +1,16 @@
-package com.roc.distributedsession.config;public class SessionConfiguration {
+package com.roc.distributedsession.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession  // 自动配置 spring session 使用 redis 作为数据源
+public class SessionConfiguration {
+    @Bean(name = "springSessionDefaultRedisSerializer")
+    public RedisSerializer springSessionDefaultRedisSerializer() {
+        return RedisSerializer.json();
+    }
+
 }
--- distributed-session-01/src/main/java/com/roc/distributedsession/controller/SessionController.java
+++ distributed-session-01/src/main/java/com/roc/distributedsession/controller/SessionController.java
@@ -1,2 +1,38 @@
-package com.roc.distributedsession.controller;public class SessionController {
+package com.roc.distributedsession.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+    @GetMapping("/set")
+    public String set(HttpSession session,
+                    @RequestParam("key") String key,
+                    @RequestParam("value") String value) {
+        session.setAttribute(key, value);
+        return "access success";
+    }
+
+    @GetMapping("/get_all")
+    public Map<String, Object> getAll(HttpSession session) {
+        Map<String, Object> result = new HashMap<>();
+
+        for (Enumeration<String> enumeration = session.getAttributeNames();
+             enumeration.hasMoreElements();
+        ) {
+            String key = enumeration.nextElement();
+            Object value = session.getAttribute(key);
+            result.put(key, value);
+        }
+        return result;
+    }
 }
